3e876afb3de041395b9a19a1ca43b055e84094c9
unknown <Student@DIGITALSCHOOL.LOCAL>
2025-10-08 20:40:57 +0200

    multidimensional & associative arrays and nested loops

---
+++ moduli_5/multidimentionalArray.php
@@ -0,0 +1,11 @@
+<?php
+$dogs = array (
+    array ("Doberman", "Germany", 20),
+    array ("Husky", "Mexico", 15),
+    array ("Pitbull", "Albania", 10)
+);
+echo $dogs[0][0]. ": Origin: " .$dogs[0][1]. ",Life Span: " . $dogs[0][2]. ".<br>";
+echo $dogs[1][0]. ": Origin: " .$dogs[1][1]. ",Life Span: " . $dogs[1][2]. ".<br>";
+echo $dogs[2][0]. ": Origin: " .$dogs[2][1]. ",Life Span: " . $dogs[2][2]. ".<br>";
+
+?>
