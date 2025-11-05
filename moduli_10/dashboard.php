f4b8440e1b6fe148ab056a09a8733450d216add7
unknown <Student@DIGITALSCHOOL.LOCAL>
2025-11-05 20:41:43 +0100

    dashboard

--- moduli_10/dashboard.php
+++ moduli_10/dashboard.php
@@ -16,16 +16,15 @@
 
 </head>
 <body>
-    <?php
-    include 'db.php';
-    $sql = "SELECT * FROM users";
-    $getUsers = $conn->prepare($sql);
-    $getUsers->execute();
-    $users = $getUsers->fetchAll();
-
+  <?php
+        include_once('config.php');
+        $sql = "SELECT * FROM users";
+        $getUsers = $conn->prepare($sql);
+        $getUsers->execute();
+        $users=$getUsers->fetchAll();
     ?>
+    <br> <br>
 
-    <br>
 
     <table>
         <thead>
@@ -33,11 +32,23 @@
             <th>Name</th>
             <th>Surname</th>
             <th>Email</th>
-            
         </thead>
-    
-
+    <tbody>
+        <?php
+            foreach($users as $user){
+?> 
+    <tr>
+        <td> <?= $user['id'] ?> </td>
+        <td> <?= $user['name'] ?> </td>
+        <td> <?= $user['surname'] ?> </td>
+        <td> <?= $user['email'] ?></td>
+    </tr>
+    <?php
+            }
+    ?>        
+        
+    </tbody>
     </table>
-    
+    <a href="add.php">Add User</a>
 </body>
 </html>
