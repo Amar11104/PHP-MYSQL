909e0b0913a305ad0e731fb687c0fa37f443613f
unknown <Student@digitalschool.local>
2025-11-19 20:43:28 +0100

    signup

--- moduli_12/config.php
+++ moduli_12/config.php
@@ -1,39 +1,16 @@
 <?php
+session_start();
 
-	include_once('config.php');	
+$user="root";
+$pass="";
+$server="localhost";
+$dbname="db";
 
+try {
+	
+	$conn =new PDO("mysql:host=$server;dbname=$dbname",$user,$pass);
 
-	if(isset($_POST['submit']))
-	{
-		$name = $_POST['name'];
-		$surname = $_POST['password'];
-		$email = $_POST['email'];
-
-		
-        $sql = "insert into users (name, password, email) values (:name, :password, :email)";
-        $sqlQuery = $conn->prepare($sql);
-    
-        $sqlQuery->bindParam(':name', $name); 
-        $sqlQuery->bindParam(':password', $password); 
-        $sqlQuery->bindParam(':email', $email);
-
-        $sqlQuery->execute();
-
-        echo "Data saved successfully ...";
-	}
-?>
-
-<!DOCTYPE html>
-<html>
-    <head></head>
-    <body>
-        <a href="dashboard.php">Dashboard</a>
-        <form action="add.php" method="POST">
-            <input type="text" name="name" placeholder="Name"></br>
-            <input type="password" name="password" placeholder="Password"></br>
-            <input type="email" name="email" placeholder="Email"></br>
-            
-        </form>
-
-    </body>
-</html>
+} catch (PDOException $e) {
+	echo "error: " . $e->getMessage();
+}
+ ?>
